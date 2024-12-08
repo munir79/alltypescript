41236ce5557dcdb760dc304cbb3008de3e3c095e
munir79 <jakirhossainmunir79@gmail.com>
2024-12-08 18:09:51 +0600

    type alias

---
+++ module1/alias/alias.ts
@@ -0,0 +1,24 @@
+{
+  
+  type Student ={
+    name:string;
+    id:number;
+    email:string;
+    age:number;
+    contact ?:number;
+
+  }  
+
+
+  const student:Student={
+    name:"jakir",
+    id:1,
+    email:"jakirhossainmunir79@gmail.com",
+    age:12,
+    contact:123
+
+  }
+    
+
+
+}
